d645aeaef7edfe0bbc0f63544aae460a5652eadf
cksajil <sajilck@gmail.com>
2023-10-05 10:55:14 +0530

    Added basic files

--- eda_rainfall.py
+++ eda_rainfall.py
@@ -10,30 +10,28 @@ OUTPUT_DIR = "output_wav"
 
 col_names = ["raw_audio", "filtered_audio"]
 file_path = join(RECORDING_DIR, "doc5.csv")
-data = pd.read_csv(file_path, names=col_names).astype(float) 
-
+data = pd.read_csv(file_path, names=col_names).astype(float)
 
 print(data.dtypes)
-
 
 raw_audio = data["raw_audio"].to_numpy()
 filtered_audio = data["filtered_audio"].to_numpy()
 
 
 def normalize_signal(x):
-    x /= np.max(np.abs(x),axis=0)
+    x /= np.max(np.abs(x), axis=0)
     x -= 0.5
     return x
 
 
-Fs = 9600 # 115200
+Fs = 9600  # 115200
 raw_audio = normalize_signal(raw_audio)
 filtered_audio = normalize_signal(filtered_audio)
 N = len(raw_audio)
-time = np.linspace(0, N/Fs, N)
+time = np.linspace(0, N / Fs, N)
 
 
-plt.figure(figsize=(15,4))
+plt.figure(figsize=(15, 4))
 plt.plot(time, raw_audio, label="raw_audio")
 plt.plot(time, filtered_audio, label="filterd_audio")
 plt.xlabel("time")
@@ -43,4 +41,3 @@ plt.legend()
 
 wav_file_path = join(OUTPUT_DIR, "test_raw.wav")
 write(wav_file_path, Fs, raw_audio)
-
