b96531e7f134415f08f9eac587edb41a81f1a384
cksajil <sajilck@gmail.com>
2023-10-31 12:51:18 +0530

    update reading code

--- arduino/audio_read.ino
+++ arduino/audio_read.ino
@@ -1,9 +1,10 @@
 void setup() {
   pinMode(WIO_MIC, INPUT);
   Serial.begin(2000000);
+  
 }
-
-void loop() {
-  int val = analogRead(WIO_MIC);
-  Serial.println(val);
-}
+  void loop()
+  {  
+     int val = analogRead(WIO_MIC);
+     Serial.println(val);          
+  }
