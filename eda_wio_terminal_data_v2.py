19971ae9f150c1ec0826c7b0e7f20396c57762db
cksajil <sajilck@gmail.com>
2023-10-31 15:29:03 +0530

    update wio audio convert

--- eda_wio_terminal_data_v2.py
+++ eda_wio_terminal_data_v2.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from tqdm import tqdm
 from os.path import join
+from scipy.io.wavfile import write
 
 RECORDING_DIR = "recordings"
 WIO_PROCESSED_FILE = "wio_processed.csv"
 WIO_TERMINAL_FILE = "sound_log.txt"
+AUDIO_FILE = "sample_audio.wav"
+Fs = 16000
 
 with open(WIO_TERMINAL_FILE) as file:
     data = file.readlines()
@@ -28,3 +31,5 @@ df["raw_audio"] = raw_audio
 df.to_csv(join(RECORDING_DIR, WIO_PROCESSED_FILE), index=False)
 
 print(df["timestamp"].value_counts())
+
+write(join(RECORDING_DIR, AUDIO_FILE), Fs, df["raw_audio"].to_numpy())
