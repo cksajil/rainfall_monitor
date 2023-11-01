3af5cc59d19d587d3e8a14d6251c202c826d2af3
cksajil <sajilck@gmail.com>
2023-11-01 16:43:19 +0530

    renamed files

--- process_wio_terminal_data.py
+++ process_wio_terminal_data.py
@@ -5,8 +5,9 @@ from os.path import join
 from scipy.io.wavfile import write
 
 RECORDING_DIR = "recordings"
+OUTPUT_DIR = "output_wav"
 WIO_PROCESSED_FILE = "wio_processed.csv"
-WIO_TERMINAL_FILE = "sound_log.txt"
+WIO_TERMINAL_FILE = "recordings.txt"
 AUDIO_FILE = "sample_audio.wav"
 Fs = 8000
 
@@ -23,4 +24,4 @@ raw_array-=0.5
 data_normalized["raw_audio"] = raw_array
 data_normalized.to_csv(join(RECORDING_DIR, WIO_PROCESSED_FILE), index=False)
 
-write(join(RECORDING_DIR, AUDIO_FILE), Fs, raw_array)
+write(join(OUTPUT_DIR, AUDIO_FILE), Fs, raw_array)
