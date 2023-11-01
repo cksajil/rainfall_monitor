10489b3da1029a9242f5f306b895665bfdc9185f
cksajil <sajilck@gmail.com>
2023-11-01 16:27:28 +0530

    updated reconstruction code

--- eda_wio_terminal_data_v2.py
+++ eda_wio_terminal_data_v2.py
@@ -8,35 +8,19 @@ RECORDING_DIR = "recordings"
 WIO_PROCESSED_FILE = "wio_processed.csv"
 WIO_TERMINAL_FILE = "sound_log.txt"
 AUDIO_FILE = "sample_audio.wav"
-Fs = 16000
+Fs = 8000
 
-with open(WIO_TERMINAL_FILE) as file:
-    data = file.readlines()
-
-data = data[:-1:2]
-
-raw_audio = []
-timestamps = []
-
-df = pd.DataFrame()
-
-for row in tqdm(data):
-    timestamp = " ".join(row.split(" ")[:-1])
-    reading = int(row.split(" ")[-1])
-    raw_audio.append(reading)
-    timestamps.append(timestamp)
-   
-raw_array = np.array(raw_audio)
+data = pd.read_csv(WIO_TERMINAL_FILE, names=["timestamp", "audio"])
+data_normalized = pd.DataFrame()
+data["audio"] = data["audio"].astype(float)
+raw_array = data["audio"]
 
 xmin = np.min(raw_array)
 xmax = np.max(raw_array)
 raw_array = (raw_array-xmin)/(xmax-xmin)
 raw_array-=0.5
 
-df["timestamp"] = timestamps
-df["raw_audio"] = raw_array
-
-df.to_csv(join(RECORDING_DIR, WIO_PROCESSED_FILE), index=False)
-
+data_normalized["raw_audio"] = raw_array
+data_normalized.to_csv(join(RECORDING_DIR, WIO_PROCESSED_FILE), index=False)
 
 write(join(RECORDING_DIR, AUDIO_FILE), Fs, raw_array)
