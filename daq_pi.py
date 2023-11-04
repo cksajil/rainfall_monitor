5005b539b00f02d09fb5ce6458bb7b53e1422205
cksajil <sajilck@gmail.com>
2023-11-04 16:22:25 +0530

    updated daq_pi

--- daq_pi.py
+++ daq_pi.py
@@ -1,3 +1,4 @@
+import subprocess
 from os import path
 from datetime import datetime
 from utils.helper import time_stamp_fnamer
@@ -5,11 +6,23 @@ from utils.helper import load_config
 
 
 config = load_config("config.yaml")
-
 dt_now = datetime.now()
-print("Started data logging at", dt_now)
 
+print("Started data logging at", dt_now)
 dt_fname = time_stamp_fnamer(dt_now)
 
-with open(path.join(config["data_dir"], dt_fname), "w") as f:
-    f.write("sample data")
+d = 60
+file_format = "wav"
+resolution = "S32"
+sampling_rate = "48000"
+location = config["data_dir"]
+
+subprocess.call(["arecord", 
+                 "--duration="+str(d), 
+                 "-t", file_format,
+                 "-f", resolution,
+                 "-r", sampling_rate,
+                 location])
+
+# with open(path.join(config["data_dir"], dt_fname), "w") as f:
+#     f.write("sample data")
