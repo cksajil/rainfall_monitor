060d60da51780567e3a3cb676db3662bb838db8e
cksajil <sajilck@gmail.com>
2023-11-04 15:41:00 +0530

    updated daq code

--- daq_pi.py
+++ daq_pi.py
@@ -1,1 +1,8 @@
-print("Welcome to pi")
+from datetime import datetime
+from utils.helper import time_stamp_fnamer
+
+dt_now = datetime.now()
+print("Started data logging at", dt_now)
+
+dt_fname = time_stamp_fnamer(dt_now)
+print(dt_fname)
