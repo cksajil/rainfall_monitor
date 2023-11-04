1ce1a45b8ced6a15d7d7ce6563b1fa266283d2f9
cksajil <sajilck@gmail.com>
2023-11-04 16:05:33 +0530

    basic data save functionality

--- daq_pi.py
+++ daq_pi.py
@@ -1,8 +1,15 @@
+from os import path
 from datetime import datetime
 from utils.helper import time_stamp_fnamer
+from utils.helper import load_config
+
+
+config = load_config("config.yaml")
 
 dt_now = datetime.now()
 print("Started data logging at", dt_now)
 
 dt_fname = time_stamp_fnamer(dt_now)
-print(dt_fname)
+
+with open(path.join(config["data_dir"], dt_fname), "w") as f:
+    f.write("sample data")
--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,11 @@
+import os
+import yaml
+
+
 def time_stamp_fnamer(tstamp):
+    """
+    A function to generate filenames from timestamps
+    """
     cdate, ctime =  str(tstamp).split(" ")
     current_date = "_".join(cdate.split("-"))
     chour, cmin, csec = ctime.split(":") 
@@ -6,3 +13,21 @@ def time_stamp_fnamer(tstamp):
     current_time = "_".join([chour, cmin, csec, cmilli])
     current_date_time_name = "_".join([current_date, current_time])
     return current_date_time_name
+
+
+
+def load_config(config_name):
+    """
+    A function to load and return config file in YAML format
+    """
+    CONFIG_PATH = "./config/"
+    with open(os.path.join(CONFIG_PATH, config_name)) as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+def create_folder(directory):
+    """Function to create a folder in a location if it does not exist"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
