d311b42dba2fd1b2faee8dcbceef44cf21c01d30
cksajil <sajilck@gmail.com>
2023-11-06 09:15:11 +0530

    fix path issues

--- runme.sh
+++ runme.sh
@@ -2,8 +2,8 @@
 
 login_time=$(date)
 echo "System booted up at: $login_time"
-source /home/pi/raingauge/code/ven/bin/activate
-python daq_pi.py
+source /home/pi/raingauge/code/venv/bin/activate
+python /home/pi/raingauge/code/daq_pi.py
 
 stop_time=$(date)
 echo "Data aquisition finished at: $login_time"
--- utils/helper.py
+++ utils/helper.py
@@ -20,7 +20,7 @@ def load_config(config_name):
     """
     A function to load and return config file in YAML format
     """
-    CONFIG_PATH = "./config/"
+    CONFIG_PATH = "/home/pi/raingauge/code/config/"
     with open(os.path.join(CONFIG_PATH, config_name)) as file:
         config = yaml.safe_load(file)
 
