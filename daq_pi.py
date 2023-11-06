530be1a9e1e1ae535898bb3c98a999a7da3b2af9
cksajil <sajilck@gmail.com>
2023-11-06 20:44:27 +0530

    Added logger

--- daq_pi.py
+++ daq_pi.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 from os import path
 from datetime import datetime, timedelta
@@ -6,27 +7,30 @@ from utils.helper import load_config
 
 
 config = load_config("config.yaml")
+logging.basicConfig(
+    filename=path.join(config["log_dir"], "log.txt"),
+    format="%(message)s",
+    filemode="w",
+)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 d = config["sample_duration_sec"]
 file_format = config["file_format"]
 resolution = config["resolution"]
 sampling_rate = config["sampling_rate"]
 record_hours = config["record_hours"]
-
 num_samples = int(config["record_hours"] * (3600 / d))
 
 dt_start = datetime.now()
 dt_stop = dt_start + timedelta(hours=record_hours)
 
-# f = open(path.join(config["log_dir"], "log.txt"), "w+")
-
-print("Started data logging at {}\n".format(dt_start))
-print("Total number of samples to be recorded: {}\n".format(num_samples))
-
+logger.info("Started data logging at {}\n".format(dt_start))
+logger.info("Total number of samples to be recorded: {}\n".format(num_samples))
 
 for i in range(num_samples):
     dt_now = datetime.now()
-    print("Recording sample number {} on {}".format(i, dt_now))
+    logger.info("Recording sample number {} on {}".format(i, dt_now))
     dt_fname = time_stamp_fnamer(dt_now) + ".wav"
     location = config["data_dir"] + dt_fname
 
@@ -46,15 +50,14 @@ for i in range(num_samples):
     )
 
     time_left = dt_stop - dt_now
-
     days, seconds = time_left.days, time_left.seconds
     hours = days * 24 + seconds // 3600
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
 
-    print(
+    logger.info(
         "Time left {} hours {} minutes and {} seconds\n".format(hours, minutes, seconds)
     )
+
 dt_end = datetime.now()
-print("Finished data logging at {}\n".format(dt_end))
-# f.close()
+logger.info("Finished data logging at {}\n".format(dt_end))
