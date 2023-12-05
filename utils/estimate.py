f1ca761361dd5dfb4a448d7d34f4fb2d1c0a32cc
cksajil <sajilck@gmail.com>
2023-12-05 12:49:01 +0530

    update audio trim

--- utils/estimate.py
+++ utils/estimate.py
@@ -1,6 +1,8 @@
 import librosa
 import numpy as np
 
+SEQ_LEN = 1360000
+
 
 def create_cnn_data(raw_data):
     Zxx = librosa.stft(raw_data)
@@ -10,6 +12,7 @@ def create_cnn_data(raw_data):
 
 def estimate_rainfall(model, file_path):
     audio, Fs = librosa.load(file_path)
+    audio = audio[-1 * SEQ_LEN :]
     stft_sample = create_cnn_data(audio)
     y_pred = model.predict(stft_sample, verbose=0)[0][0]
     return y_pred
