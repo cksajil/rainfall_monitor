d2b866db5bf746ec49d12f6bc620bb080724b31c
cksajil <sajilck@gmail.com>
2023-12-07 10:20:21 +0530

    update stft reshape

--- utils/estimate.py
+++ utils/estimate.py
@@ -7,7 +7,7 @@ SEQ_LEN = 1360000
 def create_cnn_data(raw_data):
     Zxx = librosa.stft(raw_data)
     stft_sample = np.abs(Zxx)
-    return stft_sample[:,:,np.newaxis]
+    return stft_sample[np.newaxis,:,:]
 
 
 def estimate_rainfall(model, file_path):
