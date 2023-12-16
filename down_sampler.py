475fa45d8d7951f6f8e20f62b95a2281143516a9
cksajil <sajilck@gmail.com>
2023-12-16 11:44:44 +0530

    update downsampler script

--- down_sampler.py
+++ down_sampler.py
@@ -1,8 +1,9 @@
+import librosa
 from tqdm import tqdm
+import soundfile as sf
 from os import listdir
 from os.path import join
-from scipy.io import wavfile
-from scipy.signal import resample
+
 
 DATA_PATH = "/Users/sajil/Downloads/rain_data_master/rainfall_sound"
 OUTPUT_PATH = "/Users/sajil/Downloads/rain_data_master/rainfall_sound_8k"
@@ -10,16 +11,10 @@ filenames = listdir(DATA_PATH)
 target_Fs = 8000
 
 
-def downsample(data, Fs, target_Fs):
-    resampled_data = resample(data, int(len(data) * target_Fs / Fs))
-    return resampled_data
-
-
 for filename in tqdm(filenames):
     filepath = join(DATA_PATH, filename)
     try:
-        Fs, audio = wavfile.read(filepath)
-        audio = downsample(audio, Fs, target_Fs)
-        wavfile.write(join(OUTPUT_PATH, filename), target_Fs, audio)
+        audio, Fs = librosa.load(filepath, sr=target_Fs)
+        sf.write(join(OUTPUT_PATH, filename), audio, target_Fs)
     except:
         print(filename)
