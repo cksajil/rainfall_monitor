882f5df881ce5c104cc06a356f6153c819e04f10
cksajil <sajilck@gmail.com>
2024-01-17 17:00:30 +0530

    format davis logger

--- davis_logger.py
+++ davis_logger.py
@@ -4,12 +4,14 @@ from gpiozero import Button
 from datetime import datetime
 from utils.helper import load_config
 
+
 config = load_config("config.yaml")
 rain_sensor = Button(6)
 BUCKET_SIZE = 0.2
 count = 0
 labels_df = pd.DataFrame(columns=["time", "rainfall"])
 dt_start = datetime.now()
+
 
 def bucket_tipped():
     print("Bucket Tipped")
@@ -27,7 +29,7 @@ while True:
     dt_now = datetime.now()
     elapsed_time = dt_now - dt_start
 
-    if elapsed_time.seconds%10 == 0:
+    if elapsed_time.seconds % 10 == 0:
         rainfall = count * BUCKET_SIZE
         labels_df.loc[len(labels_df)] = (dt_now, rainfall)
         reset_rainfall()
