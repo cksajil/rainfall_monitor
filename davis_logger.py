dd50f34ea89265eaa044c7633f5f80d26810ddfe
cksajil <sajilck@gmail.com>
2024-01-17 16:22:34 +0530

    Added davis logger script

--- davis_logger.py
+++ davis_logger.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from utils.helper import load_config
 
 config = load_config("config.yaml")
-
 rain_sensor = Button(6)
-BUCKET_SIZE = 0.2794
+BUCKET_SIZE = 0.2
 count = 0
 labels_df = pd.DataFrame(columns=["time", "rainfall"])
-
+dt_start = datetime.now()
 
 def bucket_tipped():
+    print("Bucket Tipped")
     global count
     count += 1
 
@@ -22,18 +22,14 @@ def reset_rainfall():
     count = 0
 
 
-dt_start = datetime.now()
-
 while True:
     rain_sensor.when_pressed = bucket_tipped
     dt_now = datetime.now()
     elapsed_time = dt_now - dt_start
-    dt_start = dt_now
-    if elapsed_time.seconds == 30:
+
+    if elapsed_time.seconds%10 == 0:
         rainfall = count * BUCKET_SIZE
-        labels_df = labels_df.append(
-            {"time": dt_now, "rainfall": rainfall}, ignore_index=True
-        )
+        labels_df.loc[len(labels_df)] = (dt_now, rainfall)
         reset_rainfall()
         labels_df.to_csv(
             path.join(config["log_dir"], config["label_file"]), index=False
