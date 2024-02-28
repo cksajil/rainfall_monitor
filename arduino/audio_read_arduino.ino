e327f4811820c987465506dbfd01d122b7759498
cksajil <sajildcb@gmail.com>
2024-02-28 16:22:42 +0530

    update audio reader for BLE

--- arduino/audio_read_arduino.ino
+++ arduino/audio_read_arduino.ino
@@ -34,40 +34,19 @@ void loop()
   {
     if (samplesRead) 
       { 
-        computeMelCoefficients(sampleBuffer);
+        process_audio_segment(sampleBuffer);
         // clear the read count
         samplesRead = 0;
       }
   }
 
-void computeMelCoefficients(short audio_segment[SAMPLES]) {
-    for (int i = 0; i < NUM_MEL_FILTERS; i++) {
-        for (int j = 0; j < SAMPLES / 2; j++) {
-            melFilters[i][j] = 0.0;
-        }
-    }
-
-    for (int i = 0; i < NUM_MEL_FILTERS; i++) 
-    {
-        float sum = 0.0;
-        for (int j = 0; j < SAMPLES / 2; j++) 
-          {
-            sum += audio_segment[j] * melFilters[i][j];
-          }
-
-        melCoefficients[i] = log(sum + 1);
-    }
-    for (int i = 0; i <NUM_MEL_FILTERS; i++) 
-      {Serial.println(melCoefficients[i]);}
-}
 
 
-// void process_audio_segment(short audio_segment[SAMPLES])
-//   {
-//     for (int i = 0; i < samplesRead; i++) 
-//       {Serial.println(audio_segment[i]);}
-
-//   }
+void process_audio_segment(short audio_segment[SAMPLES])
+   {
+    for (int i = 0; i < samplesRead; i++) 
+      {Serial.println(audio_segment[i]);}
+   }
 
 
 void onPDMdata() 
