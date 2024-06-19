8f031c9b9799c16a25ec0dacbe2d50c1cfb1fc24
cksajil <sajildcb@gmail.com>
2024-06-19 11:53:17 +0530

    update helper

--- utils/helper.py
+++ utils/helper.py
@@ -19,6 +19,19 @@ def load_config(config_name: str, CONFIG_PATH="./config") -> dict:
 config = load_config("config.yaml")
 
 
+def time_stamp_fnamer(tstamp) -> str:
+    """
+    A function to generate filenames from timestamps
+    """
+    cdate, ctime = str(tstamp).split(" ")
+    current_date = "_".join(cdate.split("-"))
+    chour, cmin, csec = ctime.split(":")
+    csec, cmilli = csec.split(".")
+    current_time = "_".join([chour, cmin, csec, cmilli])
+    current_date_time_name = "_".join([current_date, current_time])
+    return current_date_time_name
+
+
 def create_folder(directory: str) -> None:
     """
     Function to create a folder in a location if it does not exist
