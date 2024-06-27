cc94447f70631b23414b040c5c72b2573e66edc3
cksajil <sajildcb@gmail.com>
2024-06-27 10:47:23 +0530

    added RFM95 script

--- utils/rfm95.py
+++ utils/rfm95.py
@@ -1,48 +1,108 @@
-# https://github.com/epeters13/pyLoraRFM9x
+# Power Supply: Make sure to power the RFM95 module with 3.3V, not 5V, as the module operates at 3.3V.
+# SPI Configuration: Enable SPI on the Raspberry Pi using raspi-config.
+# GPIO Pins: Ensure no conflicts with other connected devices or GPIO configurations.
+
+# sudo pip3 install pySX127x lora-pico
+# RFM95 Pin   | Raspberry Pi Pin
+# ------------------------------
+# VCC         | 3.3V (Pin 1)
+# GND         | GND (Pin 6)
+# SCK         | GPIO11 (Pin 23)
+# MISO        | GPIO9 (Pin 21)
+# MOSI        | GPIO10 (Pin 19)
+# NSS         | GPIO8 (Pin 24)
+# RST         | GPIO25 (Pin 22)
+# DIO0        | GPIO7 (Pin 26)
+# DIO1        | GPIO18 (Pin 12) (optional)
+# DIO2        | GPIO23 (Pin 16) (optional)
+# DIO3        | GPIO24 (Pin 18) (optional)
+# DIO4        | GPIO25 (Pin 22) (optional)
 
 
-# pip install --upgrade pyLoraRFM9x
+from time import sleep
+from SX127x.LoRa import *
+from SX127x.board_config import BOARD
+from LoRaWAN.LoRaWAN import LoRaWAN
+from LoRaWAN.MHDR import MHDR
+import binascii
 
-# RFM module pin	Raspberry Pi GPIO pin
-# MISO	            MISO
-# MOSI	            MOSI
-# NSS/CS	        CE1
-# CLK	            SCK
-# RESET	            GPIO 25
-# DIO0	            GPIO 5
-# 3.3V	            3.3V
-# GND	            GND
+BOARD.setup()
 
 
-from pyLoraRFM9x import LoRa, ModemConfig
+class LoRaRFM95(LoRa):
+    def __init__(self, verbose=False):
+        super(LoRaRFM95, self).__init__(verbose)
+        self.set_mode(MODE.SLEEP)
+        self.set_dio_mapping([1, 0, 0, 0, 0, 0])
+
+    def on_tx_done(self):
+        print("TxDone")
+        self.set_mode(MODE.STDBY)
+        self.set_mode(MODE.TX)
+        self.clear_irq_flags(TxDone=1)
+
+    def send_lorawan(self, devaddr, nwkskey, appskey, data):
+        lorawan = LoRaWAN.new(nwkskey, appskey)
+        lorawan.create(
+            MHDR.UNCONF_DATA_UP,
+            {"devaddr": devaddr, "fcnt": 1, "data": list(data.encode("utf-8"))},
+        )
+        payload = lorawan.to_raw()
+        self.write_payload(payload)
+        self.set_mode(MODE.TX)
+        sleep(0.5)
+        self.set_mode(MODE.STDBY)
 
 
-# This is our callback function that runs when a message is received
-def on_recv(payload):
-    print("From:", payload.header_from)
-    print("Received:", payload.message)
-    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+# Define your device address, network session key, and application session key
+DEVADDR = [0x26, 0x01, 0x1B, 0xA1]
+NWKSKEY = [
+    0x1F,
+    0xB2,
+    0x8D,
+    0x22,
+    0xF4,
+    0xE1,
+    0xF3,
+    0x7B,
+    0x8E,
+    0x54,
+    0x7A,
+    0x67,
+    0xC6,
+    0x45,
+    0x11,
+    0x0A,
+]
+APPSKEY = [
+    0xD7,
+    0xF7,
+    0xE6,
+    0xD9,
+    0xAC,
+    0xE1,
+    0x2D,
+    0xB7,
+    0x55,
+    0x1A,
+    0xE5,
+    0x74,
+    0x91,
+    0xC7,
+    0x9E,
+    0xF2,
+]
 
+lora = LoRaRFM95(verbose=True)
+lora.set_mode(MODE.STDBY)
+lora.set_freq(868.0)
 
-# Lora object will use spi port 0 and use chip select 1. GPIO pin 5 will be used for interrupts and set reset pin to 25
-# The address of this device will be set to 2
-lora = LoRa(
-    0,
-    1,
-    5,
-    2,
-    reset_pin=25,
-    modem_config=ModemConfig.Bw125Cr45Sf128,
-    tx_power=14,
-    acks=True,
-)
-lora.on_recv = on_recv
-
-# Send a message to a recipient device with address 10
-# Retry sending the message twice if we don't get an  acknowledgment from the recipient
-message = "Hello there!"
-status = lora.send_to_wait(message, 10, retries=2)
-if status is True:
-    print("Message sent!")
-else:
-    print("No acknowledgment from recipient")
+try:
+    while True:
+        lora.send_lorawan(DEVADDR, NWKSKEY, APPSKEY, "Hello, Gateway from ICFOSS!")
+        sleep(10)
+except KeyboardInterrupt:
+    print("Interrupted")
+finally:
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
