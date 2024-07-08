d4769f92695fddf4be657300207f3ed7064a8643
cksajil <sajildcb@gmail.com>
2024-07-08 12:35:08 +0530

    update script

--- daq_pi.py
+++ daq_pi.py
@@ -1,71 +1,94 @@
 import subprocess
+from os import path
 import RPi.GPIO as GPIO
 from datetime import datetime
-from utils.helper import load_infer_model_path
-from utils.helper import time_stamp_fnamer, influxdb
-from utils.estimate import estimate_rainfall, delete_files
-from utils.helper import load_config, load_estimate_model
-from utils.gpio import setup_rain_sensor_gpio, gpio_cleanup
-from utils.gpio import enable_rain_sensor, read_rain_sensor, disable_rain_sensor
+from utils.estimate import estimate_rainfall
+from utils.helper import (
+    time_stamp_fnamer,
+    influxdb,
+    load_config,
+    delete_files,
+    load_estimate_model,
+    load_infer_model_path,
+)
+from utils.gpio import (
+    setup_rain_sensor_gpio,
+    gpio_cleanup,
+    enable_rain_sensor,
+    read_rain_sensor,
+    disable_rain_sensor,
+)
 
-config = load_config("config.yaml")
-db_counter = 0
-rain = 0
-DB_write_interval = config["DB_writing_interval_min"]
-board = config["board"]
-wav_duration = config["sample_duration_sec"]
-davis_duration = config["davis_duration_sec"]
-file_format = config["file_format"]
-resolution = config["resolution"]
-sampling_rate = config["sampling_rate"]
-record_hours = config["record_hours"]
-num_samples = int(config["record_hours"] * (3600 / wav_duration))
-num_subsamples = davis_duration // wav_duration
-infer_model_path = load_infer_model_path(board)
-interpreter = load_estimate_model(infer_model_path)
-locations = []
-# setup_rain_sensor_gpio()
-# enable_rain_sensor()
 
-for i in range(1, num_samples + 1):
-    dt_now = datetime.now()
-    print("Recording sample number {} on {}".format(i, dt_now))
-    dt_fname = time_stamp_fnamer(dt_now) + ".wav"
-    location = config["data_dir"] + dt_fname
+def record_audio(file_path, duration, file_format, resolution, sampling_rate):
     subprocess.call(
         [
             "arecord",
             "-q",
-            "--duration=" + str(wav_duration),
+            "--duration=" + str(duration),
             "-t",
             str(file_format),
             "-f",
             str(resolution),
             "-r",
-            sampling_rate,
-            location,
+            str(sampling_rate),
+            file_path,
         ]
     )
-    print("Recording finished")
-    locations.append(location)
-    model_type = config["deployed_model_type"]
-    # rain_sensor = read_rain_sensor()
-    rain_sensor = 0
-    if i % num_subsamples == 0:
-        mm_hat = estimate_rainfall(interpreter, locations)
-        delete_files(locations)
-        print("At {} model {} estimated {}".format(dt_now, model_type, mm_hat))
-        locations.clear()
 
-        rain += mm_hat
-        db_counter += 1
-        if db_counter == DB_write_interval:
-            rain_sensor_status = 0
-            if rain_sensor == GPIO.LOW and rain >= 0.6:
-                api_status = influxdb(mm_hat)
-                print(mm_hat)
-            else:
-                api_status = influxdb(0.0)
-                print(0)
 
-            print("At {} API write status: {}".format(dt_now, str(api_status)))
+def main():
+    config = load_config("config.yaml")
+    board = config["board"]
+    infer_model_path = load_infer_model_path(board)
+    db_counter, rain = 0, 0
+    DB_write_interval = config["DB_writing_interval_min"]
+    wav_duration = config["sample_duration_sec"]
+    field_deployed = config["field_deployed"]
+    infer_model = load_estimate_model(infer_model_path)
+    # setup_rain_sensor_gpio()
+    # enable_rain_sensor()
+    try:
+        if field_deployed:
+            i = 1
+            while True:
+                dt_now = datetime.now()
+                print(f"Recording sample number {i} on {dt_now}")
+                # rain_sensor = read_rain_sensor()
+                rain_sensor = 0
+                dt_fname = time_stamp_fnamer(dt_now) + ".wav"
+                location = path.join(config["data_dir"], dt_fname)
+                record_audio(
+                    location,
+                    wav_duration,
+                    config["file_format"],
+                    config["resolution"],
+                    config["sampling_rate"],
+                )
+                print(f"Recording finished for sample {i}")
+                mm_hat = estimate_rainfall(infer_model, [location])
+                delete_files([location])
+                dt_now = datetime.now()
+                print(f"At {dt_now} estimated {mm_hat}")
+
+                if rain_sensor == GPIO.LOW and rain >= 0.6:
+                    db_write_status = influxdb(mm_hat)
+                else:
+                    db_write_status = influxdb(0.0)
+                print(f"At {dt_now} DB write status {db_write_status}")
+
+                rain, db_counter = 0, 0  # Reset counters
+                i += 1
+
+    except KeyboardInterrupt:
+        # disable_rain_sensor()
+        # gpio_cleanup()
+        print("Execution interrupted by user")
+    finally:
+        pass
+        # disable_rain_sensor()
+        # gpio_cleanup()
+
+
+if __name__ == "__main__":
+    main()
