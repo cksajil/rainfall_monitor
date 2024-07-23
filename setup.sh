b4d85304f34e9f01dd9c9a2742012d4601775bdc
cksajil <sajildcb@gmail.com>
2024-07-23 17:23:38 +0530

    setup script

--- setup.sh
+++ setup.sh
@@ -67,8 +67,7 @@ mv rainfall_monitor code
 cd code/
 git checkout deployment
 cd ..
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=17yY89nn5k9YEcEXLsZiXsKorpf9Mzlvr' -O model/rain_stft.hdf5
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=15YwpKMOJ8MyvhM9zoIHB-H_u-d09p6Xz' -O model/seq_stft.hdf5
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=15rnz_j0QYxJM-4zMHGyLYQMw8a8ndjzs' -O model/seq_stft.hdf5
 echo '*********************************************************** ENVIRONMENT CREATED ***************************************************************'
 
 #installing dependencies
