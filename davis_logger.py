093aad7fa563680e4b8aa9a645c9d82c57f7b911
cksajil <sajildcb@gmail.com>
2024-08-22 14:51:55 +0530

    Added davis logger back Added file filter back  Changes to be committed: 	new file:   davis_logger.py 	new file:   file_filter.py

---
+++ davis_logger.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from os import path
+import RPi.GPIO as GPIO
+from datetime import datetime
+from utils.helper import load_config, create_folder, time_stamp_fnamer
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+from requests.exceptions import ConnectionError
+
+dt_start = datetime.now()
+config = load_config("config.yaml")
+labels_df = pd.DataFrame(columns=["time", "rainfall"])
+session_dir = time_stamp_fnamer(dt_start)
+label_dir = path.join(config["log_dir"], session_dir)
+
+create_folder(label_dir)
+BUCKET_SIZE = 0.2
+count = 0
+log_count = 0
+interrupt_pin = config["interrupt_pin"]
+logging_interval = config["davis_log_interval_sec"]
+
+
+def reset_rainfall():
+    global count
+    count = 0
+
+
+def bucket_tipped(interrupt_pin):
+    print("Bucket Tipped")
+    global count
+    count += 1
+    if count > 50:
+        reset_rainfall()
+
+
+def influxdb(rain: float):
+    """
+    function to write data to influxdb
+    """
+    try:
+        influxdb_config = load_config("influxdb_api.yaml")
+        org = influxdb_config["org"]
+        url = influxdb_config["url"]
+        bucket = influxdb_config["davis_bucket"]
+        token = influxdb_config["davis_token"]
+        client = influxdb_client.InfluxDBClient(
+            url=url, token=token, org=org, timeout=30_000
+        )
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        p = (
+            influxdb_client.Point("pi_davis_raingauge")
+            .tag("location", "greenfield tvm")
+            .field("rain", rain)
+        )
+        write_api.write(bucket=bucket, org=org, record=p)
+        client.close()
+        return True
+    except ConnectionError as e:
+        print(f"Connection to InfluxDB failed: {e}")
+        return False
+
+
+def saving_data(label_dir, dt_now):
+    rainfall = count * BUCKET_SIZE
+    influxdb(rainfall)
+    labels_df.loc[len(labels_df)] = (dt_now, rainfall)
+    labels_df.to_csv(path.join(label_dir, config["label_file"]), index=False)
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(interrupt_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(interrupt_pin, GPIO.RISING, callback=bucket_tipped, bouncetime=50)
+
+try:
+    while True:
+        dt_now = datetime.now()
+        elapsed_time = dt_now - dt_start
+        if elapsed_time.seconds % logging_interval == 0:
+            if log_count == 0:
+                saving_data(label_dir, dt_now)
+                reset_rainfall()
+                log_count = 1
+        else:
+            log_count = 0
+except KeyboardInterrupt:
+    GPIO.cleanup()
---
+++ file_filter.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+from tqdm import tqdm
+from os import listdir
+from datetime import datetime, timedelta
+
+
+MECH_FILE_PATH = "/home/icfoss/Downloads/davis_label.csv"
+
+NON_MECH_PATH = "/home/icfoss/Downloads/data/"
+
+mech_data = pd.read_csv(MECH_FILE_PATH)
+mech_data["Time"] = pd.to_datetime(mech_data["Time"])
+wave_files = sorted(listdir(NON_MECH_PATH))
+
+
+def extract_datetime_from_filename(filename):
+    timestamp_str = filename.split(".")[0]
+    return datetime.strptime(timestamp_str, "%Y_%m_%d_%H_%M_%S_%f")
+
+
+def delete_files(filtered_wavefiles, directory):
+    for wave_file in filtered_wavefiles:
+        file_path = os.path.join(directory, wave_file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        else:
+            print(f"File not found: {file_path}")
+
+
+def find_files_to_delete(mech_data, wave_files):
+    filtered_wavefiles = []
+    for wave_file in wave_files:
+        file_datetime = extract_datetime_from_filename(wave_file)
+        for checkpoint in mech_data["Time"]:
+            if checkpoint - timedelta(minutes=3) <= file_datetime <= checkpoint:
+                filtered_wavefiles.append(wave_file)
+    return list(set(wave_files) - set(filtered_wavefiles))
+
+
+delete_files(find_files_to_delete(mech_data, wave_files), NON_MECH_PATH)
