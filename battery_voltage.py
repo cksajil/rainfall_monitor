11b81d82cbebba949f82c3d5e8cc842725642387
cksajil <sajildcb@gmail.com>
2024-08-22 12:25:39 +0530

    update battery voltage reading script

--- battery_voltage.py
+++ battery_voltage.py
@@ -1,36 +1,39 @@
 import time
-import serial
-import serial.tools.list_ports
+from serial import Serial, SerialException
+from serial.tools import list_ports
+
 
 
 def list_ports():
-    """List all available ports and their descriptions."""
-    ports = list(serial.tools.list_ports.comports())
+    ports = list(list_ports.comports())
     for port in ports:
         print(
-            f"Port: {port.device}, Description: {port.description}, HWID: {port.hwid}"
-        )
+            f"Port: {port.device}, 
+            Description: {port.description}, 
+            HWID: {port.hwid}")
     return ports
 
 
-print(list_ports())
-
-# Setup UART communication
-ser = serial.Serial(
-    port="/dev/serial0",  # The serial port name
-    baudrate=9600,  # Baud rate (ensure this matches your microcontroller)
-    timeout=1,  # Timeout for read operations
-)
-
-time.sleep(2)  # Give some time for the connection to stabilize
+# print(list_ports())
 
 try:
-    while True:
-        if ser.in_waiting > 0:
-            voltage = ser.readline().decode("utf-8").strip()
-            print(f"Battery Voltage: {voltage}V")
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("Program terminated")
-finally:
-    ser.close()
+    serial_port = "/dev/serial0"
+    ser = Serial(port=serial_port,
+        baudrate=9600,
+        timeout=1)
+    print(f"Connected to {serial_port}")
+    time.sleep(2)  
+
+    try:
+        while True:
+            if ser.in_waiting > 0:
+                voltage = ser.readline().decode("utf-8").strip()
+                print(f"Battery Voltage: {voltage}V")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program terminated")
+    finally:
+        ser.close()
+
+ except SerialException as e:
+        print(f"Error: {e}")
